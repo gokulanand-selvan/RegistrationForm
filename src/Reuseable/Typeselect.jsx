57cb5a51cba69fb5e2413895eedf7a7eb7bc4774
gokul-sirpi <gokul@sirpi.io>
2023-05-04 07:59:02 +0530

    created layout

---
+++ src/Reuseable/Typeselect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Typeselect() {
+  return (
+    <div>
+        <select>
+            <option value="">Enter Sex</option>
+        </select>
+    </div>
+  )
+}
